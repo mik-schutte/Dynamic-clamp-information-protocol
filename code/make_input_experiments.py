c6cb703bdee9fbe7257a42039bbce4ccd70a0e42
Sargos-coder <mik.schutte1998@gmail.com>
2020-12-09 11:42:07 +0100

    implemented qon_qoff_type argument

--- code/make_input_experiments.py
+++ code/make_input_experiments.py
@@ -4,23 +4,21 @@
 ''' 
 from code.input import Input
 import numpy as np
+import random
 
-def make_input_experiments(baseline, amplitude_scaling, tau, factor_ron_roff, mean_firing_rate, sampling_rate, duration, seed=None):
+def make_input_experiments(qon_qoff_type, baseline, amplitude_scaling, tau, factor_ron_roff, mean_firing_rate, sampling_rate, duration, seed=None):
     '''Doc
     '''
 
-    # Parameters
-    if seed == None:
-        np.random.seed()
-        seed = np.random.rand()
-    
-    dt = 1./sampling_rate
+    np.random.seed(seed)
 
     # Fixed parameters
     N = 1000
+    dt = 1./sampling_rate
     tau_exponential_kernel = 5 #ms
     alpha = np.sqrt(1/8) # relation mean and std of the firing rates of the artificial neurons (firing rates should be positive)
     stdq = alpha*mean_firing_rate
+    
 
     ron = 1./(tau*(1+factor_ron_roff))
     roff = factor_ron_roff*ron
@@ -35,7 +33,18 @@ def make_input_experiments(baseline, amplitude_scaling, tau, factor_ron_roff, me
     input_bayes.roff = roff
     input_bayes.seed = seed
     input_bayes.xseed = seed
-    [input_bayes.qon, input_bayes.qoff] = input_bayes.create_qonqoff_balanced(N, mean_firing_rate, stdq, seed)
+
+    if qon_qoff_type == 'normal':
+        [input_bayes.qon, input_bayes.qoff] = input_bates.create_qonqoff(mutheta, N, alphan, regime, qseed)
+    elif qon_qoff_type == 'balanced':
+        [input_bayes.qon, input_bayes.qoff] = input_bayes.create_qonqoff_balanced(N, mean_firing_rate, stdq, seed)
+    elif qon_qoff_type == 'balanced_uniform':
+        minq = 100
+        maxq = 1000
+        [input_bayes.qon, input_bayes.qoff] = input_bayes.create_qonqoff_balanced_uniform(N, minq, maxq, seed)
+    else: 
+        raise SyntaxError('No qon/qoff creation type specified')
+
     input_bayes.get_all()
     input_bayes.fHandle = input_bayes.markov()
     input_bayes.generate()

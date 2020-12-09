42e0b79f58c9f4c631b65078b9868d84c72e79a3
Sargos-coder <mik.schutte1998@gmail.com>
2020-12-09 14:48:28 +0100

    added function documentation

--- code/make_input_experiments.py
+++ code/make_input_experiments.py
@@ -4,22 +4,22 @@
 ''' 
 from code.input import Input
 import numpy as np
-import random
 
 def make_input_experiments(qon_qoff_type, baseline, amplitude_scaling, tau, factor_ron_roff, mean_firing_rate, sampling_rate, duration, seed=None):
     '''Doc
     '''
 
-    np.random.seed(seed)
+    # Set RNG seed, if no seed is provided
+    if seed == None:
+        np.random.seed()
+        seed = np.random.randint(1000000000)
 
     # Fixed parameters
-    N = 1000
-    dt = 1./sampling_rate
-    tau_exponential_kernel = 5 #ms
-    alpha = np.sqrt(1/8) # relation mean and std of the firing rates of the artificial neurons (firing rates should be positive)
+    N = 1000                            
+    dt = 1./sampling_rate              
+    tau_exponential_kernel = 5 
+    alpha = np.sqrt(1/8) 
     stdq = alpha*mean_firing_rate
-    
-
     ron = 1./(tau*(1+factor_ron_roff))
     roff = factor_ron_roff*ron
 
@@ -34,8 +34,12 @@ def make_input_experiments(qon_qoff_type, baseline, amplitude_scaling, tau, fact
     input_bayes.seed = seed
     input_bayes.xseed = seed
 
+    # Create qon/qoff
     if qon_qoff_type == 'normal':
-        [input_bayes.qon, input_bayes.qoff] = input_bates.create_qonqoff(mutheta, N, alphan, regime, qseed)
+        mutheta = None
+        alphan = None
+        regime = None
+        [input_bayes.qon, input_bayes.qoff] = input_bayes.create_qonqoff(mutheta, N, alphan, regime, seed)
     elif qon_qoff_type == 'balanced':
         [input_bayes.qon, input_bayes.qoff] = input_bayes.create_qonqoff_balanced(N, mean_firing_rate, stdq, seed)
     elif qon_qoff_type == 'balanced_uniform':

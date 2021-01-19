fa1e99b10c55c00e49e091ac38080d8eb39a0185
Sargos-coder <mik.schutte1998@gmail.com>
2021-01-19 17:04:35 +0100

    separeted g and i generation

--- dynamic_clamp.py
+++ dynamic_clamp.py
@@ -4,6 +4,7 @@
     Generate an input based on a vector of conductances and more.
 '''
 import numpy as np
+from matplotlib import pyplot as plt
 
 #not for permanent use
 sampling_rate = 5      
@@ -16,29 +17,49 @@ weights = np.array(weights)
 time_vec = np.arange(dt, T+dt, dt)
 volt_vec = np.arange(-100, 75, 5)
 
-def get_conductance(volt_vec, v_rest, weights):
-    '''docstring
+def get_conductance(v_rest, weights, time_vec):
+    ''' docstring
+    sd based on A. Destexhe, M. Rudolph, J.M. Fellous & T.J. Sejnowski (2001)
     '''
     N = len(weights)
-    weight_matrix = {}
+
+    #Generate vector of conductances
+    g_dict = {}
+    for i in range(N):
+        if weights[i] > 0:
+            Er = 0
+            g0 = float(weights[i] / (-v_rest - Er))
+            sd = 0.0014
+        else: 
+            Er = -90
+            g0 = float(weights[i] / (-v_rest - Er))
+            sd = 0.0029
+
+        # Generate fluctuating conductance based on g0 and sd 
+        g_dict[i] = np.random.normal(loc=g0, scale=sd, size=len(time_vec))
+
+    #TODO are there really negative conductances?
+    #TODO If g negative but weight positive?
+    #TODO np.random.normal should be replaced by the Destexhe (2001) equation
+    return g_dict
+
+
+def get_input(volt_vec, g_dict):
+    '''docstring
+    '''
+    N = len(g_dict)
+    input_dict = {}
 
     for i in range(N):
-        weight_matrix[i] = {}
-        w = weights[i]
-
-        #Get conductance value based on g(-v_rest - Ei) = w at Vm = -65
-        if w > 0:
-            Er = 0
-            g = w / (-v_rest - Er)
-        else:
-            Er = -90
-            g = w / (-v_rest - Er)
- 
-        #Get weight for all the voltages
+        input_dict[i] = {}
         for v in volt_vec:
-            weight_matrix[i][v] = float(g * (-v - Er)) 
+            input_dict[i][v] = v * g_dict[i]
 
-    return weight_matrix
+    #TODO get all keys from g_dict in stead of for i in range(N)
+    #TODO if 0 volt input is also 0
+    #TODO we don't really have a volt for the ANN neurons.
+    return input_dict
 
-test = get_conductance(volt_vec, -65, weights)
-print(test[0][-65], weights[0])
+g_dict = get_conductance(-65, weights, time_vec)
+input_dict = get_input(volt_vec, g_dict)
+print(input_dict[0])

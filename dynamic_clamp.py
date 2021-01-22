bbcc6835c5e65b7272bd4c11ffaa8862338c2dd5
Sargos-coder <mik.schutte1998@gmail.com>
2021-01-22 13:10:06 +0100

    get_sto_cond and get_LUT now work for the ANN

--- dynamic_clamp.py
+++ dynamic_clamp.py
@@ -42,7 +42,7 @@ def get_g0(v_rest, weights):
     return g0_dict
 
 
-def get_stochastic_conductance(g0, tau, sigma, time_vec):
+def get_stochastic_conductance(g0_dict, tau, sigma, time_vec):
     ''' Generate conductance over time as a stochastic process.  
 
         INPUT:
@@ -57,22 +57,26 @@ def get_stochastic_conductance(g0, tau, sigma, time_vec):
         & T.J. Sejnowski (2001). 
     '''
     #TODO sto_cond as dict or array?
-    
     D = 2 * sigma**2 / tau                                 #Noise 'diffusion' coefficient
     h = abs(time_vec[0] - time_vec[1])                     #Integration step
     A = np.sqrt(D * tau / 2 * (1 - np.exp(-2 * h / tau) )) #Amplitude coefficient
 
     #Initiate dictionary
     sto_cond = {}
-    sto_cond[0] = g0
+    for i in g0_dict.keys():
+        g0 = g0_dict[i]
+        sto_cond[i] = {}
+        sto_cond[i][0] = g0
 
-    #Update dict following an exact update rule
-    for t in time_vec[:-1]:
-        th = round(t + h, 3) 
-        sto_cond[th] = g0 + (sto_cond[t] - g0) * np.exp(-h / tau) + A * np.random.normal()
-
-    #Return as np.array
-    return np.fromiter(sto_cond.values(), dtype=float)
+        #Update dict following an exact update rule
+        for t in time_vec[:-1]:
+            th = round(t + h, 3) 
+            sto_cond[i][th] = g0 + (sto_cond[i][t] - g0) * np.exp(-h / tau) + A * np.random.normal()
+        
+        #Un-nest dict with index as key and a list of conductances
+        sto_cond[i] = np.fromiter(sto_cond[i].values(), dtype=float)
+    
+    return sto_cond 
 
 
 def get_input_LUT(volt_vec, sto_cond, Er):
@@ -94,28 +98,20 @@ def get_input_LUT(volt_vec, sto_cond, Er):
     input_LUT = {}
 
     for v in volt_vec:
-        input_LUT[v] = sto_cond * (-v - Er)
+        input_LUT[v] = np.empty_like(sto_cond[0])
+        for i in range(N):
+            input_LUT[v] += sto_cond[i] * (-v - Er)
 
     return input_LUT
 
-def get_ANN_LUT(g0_dict, time_vec, tau, sigma, Er):
-    
-
-
 #Test 
-weights = [0.5, 0.4, 0.8, 1.2, 0.08]
+weights = [.5, 0.4, 0.8, 1.2, 0.08]
 tau = 2.7
 sigma = 0.0030
-time_vec = np.arange(0, 20, 0.2).round(3)
+time_vec = np.arange(0, 20, 1).round(3)
 volt_vec = np.arange(-100, 25, 5)
-
 g0_dict = get_g0(-65, weights)
 
-for g0 in g0_dict.values():
-    sto_cond = get_stochastic_conductance(g0, tau, sigma, time_vec)
-    
-    input_LUT = get_input_LUT(volt_vec, sto_cond, Er=0)
-    
-plt.plot(time_vec, input_LUT[-20])
-plt.show()
-
+sto_cond = get_stochastic_conductance(g0_dict, tau, sigma, time_vec)
+input_LUT = get_input_LUT(volt_vec, sto_cond, time_vec)
+print(input_LUT)

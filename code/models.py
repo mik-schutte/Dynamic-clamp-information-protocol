417ca9a379414834ecf4a8515b2cdefc1bc1efc7
Sargos-coder <mik.schutte1998@gmail.com>
2021-02-17 16:22:20 +0100

    added interneuron model

--- code/models.py
+++ code/models.py
@@ -83,13 +83,14 @@ def simulate_Wang_Buszaki(inj_input, simulation_time, clamp_type='current'):
     return M, S
 
 
-def simulate_barrel_PC(inj_input, simulation_time, clamp_type):
+def simulate_barrel_PC(inj_input, simulation_time, clamp_type, Ni=None):
     ''' Hodgkin-Huxley model of a Pyramidal Cell in the rat barrel cortex.
 
         INPUT:
             inj_input ((Tuple of) TimedArray): Input current or conductances (g_exc, g_inh)
             duration (float): Simulation time [milliseconds]
             clamp_type (string): type of input, ['current' or 'dynamic'] default = current
+            Ni (int): parameter index
 
         OUTPUT:
             StateMonitor: Brian2 StateMonitor with recorded fields
@@ -115,17 +116,23 @@ def simulate_barrel_PC(inj_input, simulation_time, clamp_type):
         tracking = ['v', 'g_exc', 'g_inh']
 
     # Neuron parameters
+    ## Pick a random set of parameters
+    parameters = np.loadtxt('PC_parameters.csv', delimiter=',')
+    if Ni == None:
+        Ni = np.random.randint(np.shape(parameters)[1])
+    
+    ## Initiate parameters
     area = 20000*b2.umetre**2
-    Cm = 1.6287344247018984e-10*b2.farad/area * b2.cm**2 
-    gL = 9.745664712129547e-09*b2.siemens/area * b2.cm**2 
-    gNa = 0.00011423100873563258*b2.siemens/area * b2.cm**2 
-    gK = 2.4881444790241606e-05*b2.siemens/area * b2.cm**2 
+    Cm = parameters[2][Ni]*b2.farad/area * b2.cm**2 
+    gL = parameters[0][Ni]*b2.siemens/area * b2.cm**2 
+    gNa = parameters[3][Ni]*b2.siemens/area * b2.cm**2 
+    gK = parameters[1][Ni]*b2.siemens/area * b2.cm**2 
     EL = -65*b2.mV
     ENa = 50*b2.mV
     EK = -90*b2.mV
-    k_m = 0.004400673179014958*b2.volt
-    k_h = 0.008439593401769845*b2.volt
-    Vh_h = -0.02973884050950356*b2.volt
+    k_m = parameters[4][Ni]*b2.volt
+    k_h = parameters[5][Ni]*b2.volt
+    Vh_h = parameters[6][Ni]*b2.volt
     VT = -63*b2.mV
     
     # Model the neuron with differential equations
@@ -161,11 +168,86 @@ def simulate_barrel_PC(inj_input, simulation_time, clamp_type):
     return M
 
 
+def simulate_barrel_IN(inj_input, simulation_time, clamp_type, Ni=None):
+    ''' Hodgkin-Huxley model of an Inter neuron in the rat barrel cortex.
 
-# Test
-# inj_input = np.genfromtxt('results/input_current.csv')
-# inj_input = b2.TimedArray(inj_input*b2.uamp, dt=0.2*b2.ms)
-# simulation_time = 2000*b2.ms
+        INPUT:
+            inj_input ((Tuple of) TimedArray): Input current or conductances (g_exc, g_inh)
+            duration (float): Simulation time [milliseconds]
+            clamp_type (string): type of input, ['current' or 'dynamic'] default = current
+            Ni (int): parameter index
 
-# monitor = simulate_barrel_PC(inj_input, simulation_time, clamp_type='current')
-# print(monitor)
+        OUTPUT:
+            StateMonitor: Brian2 StateMonitor with recorded fields
+            ['v', 'input' or 'conductance']
+
+        The parameters used in this model have been fitted by Xenia Sterl under 
+        the supervision of Fleur Zeldenrust. Full description can be found at:
+        Xenia Sterl, Fleur Zeldenrust, (2020). Dopamine modulates firing rates and information
+        transfer in inhibitory and excitatory neurons of rat barrel cortex, but shows no clear
+        influence on neuronal parameters. (Unpublished bachelor's thesis)
+    '''
+
+    # Determine the simulation
+    if clamp_type == 'current':
+        eqs_input = '''I_inj = inj_input(t) : amp'''
+        tracking = ['v', 'I_inj']
+
+    elif clamp_type =='dynamic':
+        g_exc, g_inh = inj_input
+        eqs_input = '''I_exc = g_exc(t) * (0*mV - v) : amp
+                 I_inh = g_inh(t) * (-75*mV - v) : amp
+                 I_inj = I_exc + I_inh : amp'''
+        tracking = ['v', 'g_exc', 'g_inh']
+
+    # Neuron parameters
+    ## Pick a random set of parameters
+    parameters = np.loadtxt('IN_parameters.csv', delimiter=',')
+    if Ni == None:
+        Ni = np.random.randint(np.shape(parameters)[1])
+    
+    ## Initiate parameters
+    area = 20000*b2.umetre**2
+    Cm = parameters[2][Ni]*b2.farad/area * b2.cm**2 
+    gL = parameters[0][Ni]*b2.siemens/area * b2.cm**2 
+    gNa = parameters[3][Ni]*b2.siemens/area * b2.cm**2 
+    gK = parameters[1][Ni]*b2.siemens/area * b2.cm**2 
+    EL = -65*b2.mV
+    ENa = 50*b2.mV
+    EK = -90*b2.mV
+    k_m = parameters[4][Ni]*b2.volt
+    k_h = parameters[5][Ni]*b2.volt
+    Vh_h = parameters[6][Ni]*b2.volt
+    VT = -63*b2.mV
+    
+    # Model the neuron with differential equations
+    eqs = '''
+        Vh_m = 3.583881 * k_m - 53.294454*mV : volt
+        m = 1 / (1 + exp(-(v - Vh_m) / k_m)) : 1
+        h = 1 / (1 + exp((v - Vh_h) / k_h)) : 1
+
+        alpha_n = (0.032 * 5. / exprel((15. -v/mV + VT/mV) / 5.))/ms : Hz
+        beta_n = (0.5 * exp((10. - v/mV + VT/mV) / 40.))/ms : Hz
+        dn/dt = alpha_n * (1 - n) - beta_n * n : 1
+
+        I_leak = gL * (v - EL) : amp
+        I_Na = gNa * m**3 * h * (v - ENa) : amp
+        I_K = gK * n**4 * (v - EK) : amp
+
+        dv/dt = (-(I_leak + I_Na + I_K) + I_inj) / Cm : volt
+        '''    
+
+    # Neuron & parameter initialization
+    neuron = b2.NeuronGroup(1, model=eqs+eqs_input, method='exponential_euler',
+                        threshold ='m > 0.5', refractory=2*b2.ms, reset=None, dt=0.5*b2.ms)
+    neuron.v = -65*b2.mV
+
+    # Track the parameters during simulation
+    M = b2.StateMonitor(neuron, tracking, record=True)
+
+    # Run the simulation
+    net = b2.Network(neuron)
+    net.add(M)
+    net.run(simulation_time, report='text')
+
+    return M
